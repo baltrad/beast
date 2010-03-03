9eed502f334d8931a41415426ddc08be31eb930e
Anders <anders@studio16-linux.(none)>
2010-03-03 10:41:28 +0100

    Added BltGeneration message

---
+++ src/eu/baltrad/beast/message/mo/BltGenerationMessage.java
@@ -0,0 +1,149 @@
+/* --------------------------------------------------------------------
+Copyright (C) 2009-2010 Swedish Meteorological and Hydrological Institute, SMHI,
+
+This file is part of the Beast library.
+
+Beast library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Beast library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Beast library library.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------*/
+package eu.baltrad.beast.message.mo;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Node;
+
+import eu.baltrad.beast.message.IBltMessage;
+import eu.baltrad.beast.message.IBltXmlMessage;
+import eu.baltrad.beast.message.MessageParserException;
+
+/**
+ * @author Anders Henja
+ */
+public class BltGenerationMessage implements IBltMessage, IBltXmlMessage {
+  public static final String BLT_GENERATE = "bltgenerate";
+  
+  /**
+   * The algorithm identifier.
+   */
+  private String algorithm = null;
+  
+  /**
+   * The file(s) that should be used.
+   */
+  private String[] files = new String[]{}; 
+                 
+  /**
+   * The arguments
+   */
+  private String[] arguments = new String[]{};
+  
+  /**
+   * @param algorithm the algorithm to set
+   */
+  public void setAlgorithm(String algorithm) {
+    this.algorithm = algorithm;
+  }
+
+  /**
+   * @return the algorithm
+   */
+  public String getAlgorithm() {
+    return algorithm;
+  }
+
+  /**
+   * @param files the files to set
+   */
+  public void setFiles(String[] files) {
+    if (files == null) {
+      throw new IllegalArgumentException("files must not be null");
+    }
+    this.files = files;
+  }
+
+  /**
+   * @return the files
+   */
+  public String[] getFiles() {
+    return files;
+  }
+
+  /**
+   * @param arguments the arguments to set
+   */
+  public void setArguments(String[] arguments) {
+    if (arguments == null) {
+      throw new IllegalArgumentException("arguments must not be null");
+    }    
+    this.arguments = arguments;
+  }
+
+  /**
+   * @return the arguments
+   */
+  public String[] getArguments() {
+    return arguments;
+  }
+  
+  /**
+   * @see eu.baltrad.beast.message.IBltXmlMessage#fromDocument(org.dom4j.Document)
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void fromDocument(Document dom) {
+    int index = 0;
+    if (!dom.getRootElement().getName().equals(BLT_GENERATE)) {
+      throw new MessageParserException("Atempting to create BltGenerate from: " + dom.asXML());
+    }
+    setAlgorithm(dom.valueOf("//bltgenerate/algorithm"));
+    List<Node> nodes = dom.selectNodes("//bltgenerate/filelist/file");
+    this.files = new String[nodes.size()];
+    for (Node node : nodes) {
+      this.files[index++] = node.getText();
+    }
+    
+    index = 0;
+    nodes = dom.selectNodes("//bltgenerate/arguments/arg");
+    this.arguments = new String[nodes.size()];
+    for (Node node : nodes) {
+      this.arguments[index++] = node.getText();
+    }
+  }
+  
+  /**
+   * @see eu.baltrad.beast.message.IBltXmlMessage#toDocument()
+   */
+  @Override
+  public Document toDocument() {
+    Document document = DocumentHelper.createDocument();
+    Element el = document.addElement(BLT_GENERATE);
+    el.addElement("algorithm").addText(this.algorithm);
+    Element elFiles = el.addElement("filelist");
+    for (int i = 0; this.files != null && i < this.files.length; i++) {
+      if (this.files[i] != null) {
+        elFiles.addElement("file").addText(this.files[i]);
+      }
+    }
+    
+    Element elArgs = el.addElement("arguments");
+    for (int i = 0; this.arguments != null && i < this.arguments.length; i++) {
+      if (this.arguments[i] != null) {
+        elArgs.addElement("arg").addText(this.arguments[i]);
+      }
+    }
+    return document;
+  }
+}
---
+++ test/eu/baltrad/beast/message/mo/BltGenerationMessageTest.java
@@ -0,0 +1,166 @@
+/* --------------------------------------------------------------------
+Copyright (C) 2009-2010 Swedish Meteorological and Hydrological Institute, SMHI,
+
+This file is part of the Beast library.
+
+Beast library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Beast library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Beast library library.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------*/
+package eu.baltrad.beast.message.mo;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Anders Henja
+ */
+public class BltGenerationMessageTest extends TestCase {
+  public void testSetAlgorithm() {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    assertEquals(null, classUnderTest.getAlgorithm());
+    classUnderTest.setAlgorithm("a.b");
+    assertEquals("a.b", classUnderTest.getAlgorithm());
+  }
+  
+  public void testSetFiles() {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    String[] result = classUnderTest.getFiles();
+    assertEquals(0, result.length);
+    classUnderTest.setFiles(new String[]{"a","b"});
+    result = classUnderTest.getFiles();
+    assertEquals(2, result.length);
+    assertEquals("a", result[0]);
+    assertEquals("b", result[1]);
+  }
+
+  public void testSetFiles_null() {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    try {
+      classUnderTest.setFiles(null);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+      // pass
+    }
+  }
+
+  public void testSetArguments() {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    String[] result = classUnderTest.getArguments();
+    assertEquals(0, result.length);
+    classUnderTest.setArguments(new String[]{"a","b"});
+    result = classUnderTest.getArguments();
+    assertEquals(2, result.length);
+    assertEquals("a", result[0]);
+    assertEquals("b", result[1]);
+  }
+  
+  public void testSetArguments_null() {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    try {
+      classUnderTest.setArguments(null);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+      // pass
+    }
+  }
+
+  public void testFromDocument() throws Exception {
+    Document document = DocumentHelper.createDocument();
+    Element el = document.addElement("bltgenerate");
+    el.addElement("algorithm").addText("one.Algorithm");
+    Element subtree = el.addElement("filelist");
+    subtree.addElement("file").addText("file1");
+    subtree.addElement("file").addText("file2");
+    
+    subtree = el.addElement("arguments");
+    subtree.addElement("arg").addText("arg1");
+    subtree.addElement("arg").addText("arg2");
+
+    // execute
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    classUnderTest.fromDocument(document);
+    
+    // verify
+    assertEquals("one.Algorithm", classUnderTest.getAlgorithm());
+    String[] files = classUnderTest.getFiles();
+    assertEquals(2, files.length);
+    assertEquals("file1", files[0]);
+    assertEquals("file2", files[1]);
+    String[] args = classUnderTest.getArguments();
+    assertEquals(2, args.length);
+    assertEquals("arg1", args[0]);
+    assertEquals("arg2", args[1]);
+  }
+  
+  public void testFromDocument_nofiles() throws Exception {
+    Document document = DocumentHelper.createDocument();
+    Element el = document.addElement("bltgenerate");
+    el.addElement("algorithm").addText("one.Algorithm");
+    
+    Element subtree = el.addElement("arguments");
+    subtree.addElement("arg").addText("arg1");
+    subtree.addElement("arg").addText("arg2");
+
+    // execute
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    classUnderTest.fromDocument(document);
+    
+    // verify
+    assertEquals("one.Algorithm", classUnderTest.getAlgorithm());
+    String[] files = classUnderTest.getFiles();
+    assertEquals(0, files.length);
+    String[] args = classUnderTest.getArguments();
+    assertEquals(2, args.length);
+    assertEquals("arg1", args[0]);
+    assertEquals("arg2", args[1]);
+  }  
+  
+  public void testFromDocument_noArguments() throws Exception {
+    Document document = DocumentHelper.createDocument();
+    Element el = document.addElement("bltgenerate");
+    el.addElement("algorithm").addText("one.Algorithm");
+    Element subtree = el.addElement("filelist");
+    subtree.addElement("file").addText("file1");
+    subtree.addElement("file").addText("file2");
+    
+    // execute
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    classUnderTest.fromDocument(document);
+    
+    // verify
+    assertEquals("one.Algorithm", classUnderTest.getAlgorithm());
+    String[] files = classUnderTest.getFiles();
+    assertEquals(2, files.length);
+    assertEquals("file1", files[0]);
+    assertEquals("file2", files[1]);
+    String[] args = classUnderTest.getArguments();
+    assertEquals(0, args.length);
+  }
+  
+  public void testToDocument() throws Exception {
+    BltGenerationMessage classUnderTest = new BltGenerationMessage();
+    classUnderTest.setAlgorithm("some.Algorithm");
+    classUnderTest.setFiles(new String[]{"a", "b"});
+    classUnderTest.setArguments(new String[]{"-k","val"});
+    Document result = classUnderTest.toDocument();
+    assertEquals("bltgenerate", result.getRootElement().getName());
+    assertEquals("some.Algorithm", result.valueOf("//bltgenerate/algorithm"));
+    assertEquals("a", result.valueOf("//bltgenerate/filelist/file[1]"));
+    assertEquals("b", result.valueOf("//bltgenerate/filelist/file[2]"));
+    assertEquals("-k", result.valueOf("//bltgenerate/arguments/arg[1]"));
+    assertEquals("val", result.valueOf("//bltgenerate/arguments/arg[2]"));
+  }
+}
