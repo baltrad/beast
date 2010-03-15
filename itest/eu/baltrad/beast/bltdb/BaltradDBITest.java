e21fd32763e3e5ec7b132c049ea5599c83657952
Anders <anders@studio16-linux.(none)>
2010-03-15 21:46:03 +0100

    Added some integration tests for the baltrad-db API

--- itest/eu/baltrad/beast/bltdb/BaltradDBITest.java
+++ itest/eu/baltrad/beast/bltdb/BaltradDBITest.java
@@ -18,14 +18,22 @@ along with the Beast library library.  If not, see <http://www.gnu.org/licenses/
 ------------------------------------------------------------------------*/
 package eu.baltrad.beast.bltdb;
 
-import eu.baltrad.fc.oh5.File;
+import java.io.FilenameFilter;
+import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 import eu.baltrad.beast.itest.BeastDBTestHelper;
 import eu.baltrad.fc.FileCatalog;
+import eu.baltrad.fc.Query;
+import eu.baltrad.fc.ResultSet;
+import eu.baltrad.fc.expr.Expression;
+import eu.baltrad.fc.expr.ExpressionFactory;
+import eu.baltrad.fc.oh5.File;
 
 /**
  * @author Anders Henja
@@ -34,20 +42,193 @@ import eu.baltrad.fc.FileCatalog;
 public class BaltradDBITest extends TestCase {
   private ApplicationContext context = null;
   private BeastDBTestHelper helper = null;
-  private static String h5fixture = "ODIM_H5_pvol_ang_20090501T1200Z.h5";
+  private FileCatalog catalogue = null;
+  private String baltradDbPath = null;
+  
+  private static String[] FIXTURES = {
+    "fixtures/pvol_seang_20090501T120000Z.h5",
+    "fixtures/pvol_searl_20090501T120000Z.h5",
+    "fixtures/pvol_sease_20090501T120000Z.h5",
+    "fixtures/pvol_sehud_20090501T120000Z.h5",
+    "fixtures/pvol_sekir_20090501T120000Z.h5",
+    "fixtures/pvol_sekkr_20090501T120000Z.h5",
+    "fixtures/pvol_selek_20090501T120000Z.h5",
+    "fixtures/pvol_selul_20090501T120000Z.h5",
+    "fixtures/pvol_seosu_20090501T120000Z.h5",
+    "fixtures/pvol_seovi_20090501T120000Z.h5",
+    "fixtures/pvol_sevar_20090501T120000Z.h5",
+    "fixtures/pvol_sevil_20090501T120000Z.h5"
+  };
   
   public BaltradDBITest(String name) {
     super(name);
+    //System.out.println("LIBRARYPATH: " + System.getProperty("java.library.path"));
     context = BeastDBTestHelper.loadContext(this);
     helper = (BeastDBTestHelper)context.getBean("testHelper");
+    baltradDbPath = helper.getBaltradDbPth();
+    catalogue = new FileCatalog(helper.getBaltradDbUri(), baltradDbPath);
   }
   
-  public void testSomething() throws Exception {
-    java.io.File f = new java.io.File(this.getClass().getResource(h5fixture).getFile());
-    String filename = f.getAbsolutePath();
+  /**
+   * Hack to purge the baltrad-db from all entries.
+   */
+  private void purgeBaltradDB() throws Exception {
+    String pth = helper.getBaltradDbPth();
+    FilenameFilter filter = new FilenameFilter() {
+      public boolean accept(java.io.File dir, String name) {
+        return name.endsWith(".h5");
+      }
+    };
+    java.io.File dir = new java.io.File(pth);
+    String[] list = dir.list(filter);
+    for (String n : list) {
+      (new java.io.File(pth, n)).delete();
+    }
     
-    FileCatalog fc = new FileCatalog(helper.getBaltradDbUri(), helper.getBaltradDbPth());
-    File h5f = fc.catalog(filename);
-    assertNotNull(h5f);
+    SimpleJdbcTemplate template = new SimpleJdbcTemplate(helper.getSource());
+    template.update("delete from attribute_values_bool");
+    template.update("delete from attribute_values_date");
+    template.update("delete from attribute_values_int");
+    template.update("delete from attribute_values_real");
+    template.update("delete from attribute_values_str");
+    template.update("delete from attribute_values_time");
+    template.update("delete from attribute_values_str");
+    template.update("delete from attribute_groups");
+    template.update("delete from data_objects");
+    template.update("delete from files");
   }
+  
+  private String getFilePath(String resource) throws Exception {
+    java.io.File f = new java.io.File(this.getClass().getResource(resource).getFile());
+    return f.getAbsolutePath();
+  }
+  
+  public void setUp() throws Exception {
+    purgeBaltradDB();
+    long startTime = System.currentTimeMillis();
+    for (String n : FIXTURES) {
+      File result = catalogue.catalog(getFilePath(n));
+      assertNotNull(result);
+    }
+    System.out.println("Catalogued " + FIXTURES.length + " files in " + (System.currentTimeMillis() - startTime) + "ms");
+  }
+  
+  public void tearDown() throws Exception {
+    catalogue = null;
+  }
+  
+  public void test_find_seang() throws Exception {
+    Query q = catalogue.query();
+    ExpressionFactory xpr = new ExpressionFactory();
+    q.fetch(xpr.attribute("path"));
+    q.filter(xpr.attribute("src_node").eq(xpr.string("seang")));
+    ResultSet rs = q.execute();
+    assertEquals(1, rs.size());
+    rs.next();
+    String result = rs.string(0);
+    rs.delete();
+    assertEquals(baltradDbPath+"/Z_PVOL_C_ESWI_20090501120000_seang.h5", result);
+  }
+
+  public void test_find_all() throws Exception {
+    Query q = catalogue.query();
+    ExpressionFactory xpr = new ExpressionFactory();
+    Set<String> result = new HashSet<String>();
+    
+    q.fetch(xpr.attribute("src_node"));
+
+    ResultSet rs = q.execute();
+    assertEquals(12, rs.size());
+    while (rs.next()) {
+      result.add(rs.string(0));
+    }
+    rs.delete();
+    assertTrue(result.contains("sekir"));
+    assertTrue(result.contains("selul"));
+    assertTrue(result.contains("seosu"));
+    assertTrue(result.contains("seovi"));
+    assertTrue(result.contains("sehud"));
+    assertTrue(result.contains("selek"));
+    assertTrue(result.contains("searl"));
+    assertTrue(result.contains("sease"));
+    assertTrue(result.contains("sevil"));
+    assertTrue(result.contains("sevar"));
+    assertTrue(result.contains("seang"));
+    assertTrue(result.contains("sekkr"));
+  }
+  
+  public void Xtest_find_sekir_or_selul() throws Exception {
+    Query q = catalogue.query();
+    ExpressionFactory xpr = new ExpressionFactory();
+    
+    q.fetch(xpr.attribute("src_node"));
+    Expression e1 = xpr.attribute("src_node").eq(xpr.string("sekir"));
+    Expression e2 = xpr.attribute("src_node").eq(xpr.string("selul"));
+    q.filter(xpr.and_(e1, e2));
+    ResultSet rs = q.execute();
+    assertEquals(2, rs.size());
+    rs.next();
+    String result1 = rs.string(0);
+    rs.next();
+    String result2 = rs.string(0);
+    rs.delete();
+    if (result1.equals("sekir")) {
+      assertEquals("selul", result2);
+    } else if (result1.equals("selul")) {
+      assertEquals("sekir", result2);
+    } else {
+      fail("Expected to get sekir and selul");
+    }
+  }
+  
+  public void test_find_all_but_sekir() throws Exception {
+    Query q = catalogue.query();
+    ExpressionFactory xpr = new ExpressionFactory();
+    Set<String> result = new HashSet<String>();
+    
+    q.fetch(xpr.attribute("src_node"));
+    q.filter(xpr.attribute("src_node").ne(xpr.string("sekir")));
+    ResultSet rs = q.execute();
+    assertEquals(11, rs.size());
+    while (rs.next()) {
+      result.add(rs.string(0));
+    }
+    rs.delete();
+    assertTrue(result.contains("selul"));
+    assertTrue(result.contains("seosu"));
+    assertTrue(result.contains("seovi"));
+    assertTrue(result.contains("sehud"));
+    assertTrue(result.contains("selek"));
+    assertTrue(result.contains("searl"));
+    assertTrue(result.contains("sease"));
+    assertTrue(result.contains("sevil"));
+    assertTrue(result.contains("sevar"));
+    assertTrue(result.contains("seang"));
+    assertTrue(result.contains("sekkr"));
+  }
+
+  public void test_find_elangles_searl() throws Exception {
+    Query q = catalogue.query();
+    ExpressionFactory xpr = new ExpressionFactory();
+    Set<Double> result = new HashSet<Double>();
+    
+    q.fetch(xpr.attribute("src_node"));
+    q.fetch(xpr.attribute("where/elangle"));
+    q.filter(xpr.attribute("src_node").eq(xpr.string("searl")));
+    q.filter(xpr.attribute("where/elangle").between(xpr.real(-1.0), xpr.real(5.0)));
+    
+    ResultSet rs = q.execute();
+    assertEquals(6, rs.size());
+    while (rs.next()) {
+      assertEquals("searl", rs.string(0));
+      result.add(rs.real(1));
+    }
+    rs.delete();
+    assertTrue(result.contains(0.5));
+    assertTrue(result.contains(1.0));
+    assertTrue(result.contains(1.5));
+    assertTrue(result.contains(2.0));
+    assertTrue(result.contains(2.5));
+    assertTrue(result.contains(4.0));
+  }  
 }
