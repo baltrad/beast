a6710d96104045497ebe22bcfbfa619d35a0d016
Sander Ernes <sander@baltrad.eu>
2011-03-31 09:16:47 +0300

    fix licensing information in file headers

--- src/eu/baltrad/beast/net/ftp/CommonsFTPClient.java
+++ src/eu/baltrad/beast/net/ftp/CommonsFTPClient.java
@@ -1,20 +1,20 @@
 /*
 Copyright 2009-2011 Swedish Meteorological and Hydrological Institute, SMHI,
 
-This file is part of HLHDF.
+This file is part of Beast library.
 
-HLHDF is free software: you can redistribute it and/or modify
+Beast library is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-HLHDF is distributed in the hope that it will be useful,
+Beast library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with HLHDF.  If not, see <http://www.gnu.org/licenses/>.
+along with Beast library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package eu.baltrad.beast.net.ftp;
